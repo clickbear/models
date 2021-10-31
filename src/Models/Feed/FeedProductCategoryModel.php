281e5d538394c0cab66ab23b0b9b1e862c6f8f1e
Niels Hamelink <nielshamelink@gmail.com>
2021-10-31 21:56:09 +0100

    Tables for product category & product image processing

---
+++ src/Models/Feed/FeedProductCategoryModel.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Clickbear\Models\Feed;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FeedProductCategoryModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'feed_product_category';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'product_id',
+        'image_path',
+    ];
+}
