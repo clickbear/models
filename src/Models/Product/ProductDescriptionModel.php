3ab1ca1655ef17d983c112a203c4b8c01f1780ac
Niels Hamelink <nielshamelink@gmail.com>
2021-12-02 01:31:20 +0100

    product_id update product description model

--- src/Models/Product/ProductDescriptionModel.php
+++ src/Models/Product/ProductDescriptionModel.php
@@ -24,6 +24,7 @@ class ProductDescriptionModel extends Model
      * @var array
      */
     protected $fillable = [
+        'product_id',
         'name',
         'title',
         'description',
