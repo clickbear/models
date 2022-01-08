e96a0169e81f5a22ce50e6441a0992a7e24391fc
Niels Hamelink <nielshamelink@gmail.com>
2022-01-08 14:13:47 +0100

    Product price model

---
+++ src/Models/Product/ProductPriceModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Clickbear\Models\Product;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductPriceModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'product_prices';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'product_id',
+        'supplier_id',
+        'price',
+    ];
+
+}
