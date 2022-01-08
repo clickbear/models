6cab4b06f6a6b784d7b8cd658b398fd1e8186893
Niels Hamelink <nielshamelink@gmail.com>
2022-01-08 11:20:49 +0100

    Menu models

---
+++ src/Models/Catalog/Page/MenuItemModel.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Clickbear\Models\Catalog\Page;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class MenuItemModel extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'menus';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'url',
+        'parent_id',
+        'order',
+        'route',
+        'parameters',
+    ];
+
+    /**
+     * Get the parent item
+     *
+     * @return HasOne
+     */
+    public function parent(): HasOne
+    {
+        return $this->hasOne(self::class, 'id', 'parent_id');
+    }
+
+    /**
+     * Get the related menu
+     *
+     * @return HasOne
+     */
+    public function menu(): HasOne
+    {
+        return $this->hasOne(MenuModel::class, 'id', 'menu_id');
+    }
+}
